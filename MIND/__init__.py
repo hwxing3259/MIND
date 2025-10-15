a3ef7aeadd1b7b7ecc346eeda3a3cfb95304189f
Hanwen Xing <50168252+hwxing3259@users.noreply.github.com>
2025-10-15 23:02:19 +0100

    Update __init__.py

--- MIND/__init__.py
+++ MIND/__init__.py
@@ -1,2 +1,2 @@
 from .MIND_model import MIND
-from .MIND_data import get_synthetic
+from .MIND_data import get_synthetic, load_synthetic, get_TCGA, load_TCGA, get_CCMA, load_CCMA, get_CCLE, load_CCLE
