dee955f2ac54c175bf7d01329ae545a231f0f365
Hanwen Xing <50168252+hwxing3259@users.noreply.github.com>
2025-10-15 22:24:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
                       'matplotlib~=3.8.0',
                       'torch~=2.2.2',
                       'openTSNE~=1.0.2',
-                      'request~=2.32.5',
+                      'requests~=2.32.5',
                       'zipfile~=3.13'],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
 
